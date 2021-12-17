93ef141b84a47b7ea9ffc2bffea1b93590349182
Ivan <72856588+Pipo1999@users.noreply.github.com>
2021-12-17 08:08:34 +0100

    Add files via upload

---
+++ MyOwnCinema/js/login.js
@@ -0,0 +1,7 @@
+window.onload = main();
+
+function main() {
+    document.getElementById("acceder").style = "filter:opacity(0.5);";
+    document.getElementById("acceder").setAttribute("href", "#");
+    document.getElementById("header").style = "height: 10px";
+}
---
+++ MyOwnCinema/js/registro.js
@@ -0,0 +1,7 @@
+window.onload = main();
+
+function main() {
+    document.getElementById("registrarse").style = "filter:opacity(0.5);";
+    document.getElementById("registrarse").setAttribute("href", "#");
+    document.getElementById("header").style = "height: 10px";
+}
